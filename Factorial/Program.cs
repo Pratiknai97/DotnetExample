200fcf392e42b7e40a58ebc540aa5219267d83ee
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-05-20 10:48:33 +0530

    Add Maximum and Minimum value

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -2,16 +2,16 @@
 
 using System.Security.Cryptography.X509Certificates;
 
-int num, i, multi;
+//int num, i, multi;
 
-Console.WriteLine("enter a number");
-num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("enter a number");
+//num = Convert.ToInt32(Console.ReadLine());
 
-multi = 1;
-for(i= 1; i<=num; i++)
-{
-    multi = i * multi;
-    Console.WriteLine($"{i} = {multi}");
-   }
-Console.WriteLine("hello");
-//Console.WriteLine(multi);
+//multi = 1;
+//for(i= 1; i<=num; i++)
+//{
+//    multi = i * multi;
+//    Console.WriteLine($"{i} = {multi}");
+//   }
+//Console.WriteLine("hello");
+////Console.WriteLine(multi);
---
+++ MaximumAndMinimumValue/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i, num, max = 0, mini;
+
+for (i = 1; i <= 10; i++)
+{
+    Console.WriteLine("please enter a number");
+    num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > max)
+    {
+        max = num;
+    }
+    //if (num<mini)
+    //{
+    //    mini = num;
+    //}
+}
+Console.WriteLine(max);
