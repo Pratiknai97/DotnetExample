b1dd796a95a57f89fe2915e57d81240b5847a374
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-05-22 18:03:02 +0530

    minimum value get

--- MaximumAndMinimumValue/Program.cs
+++ MaximumAndMinimumValue/Program.cs
@@ -1,13 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Globalization;
 
-int i,j, num, max = 0, mini = 0;
+int i, num, max = 0, mini = 0;
 
 for (i = 1; i <= 10; i++)
 {
     Console.WriteLine("Please enter a number:");
     num = Convert.ToInt32(Console.ReadLine());
 
+    if (i == 1)
+    {
+        mini = num;
+    }
     if (num < mini)
     {
         mini = num;
@@ -16,6 +20,6 @@ for (i = 1; i <= 10; i++)
 
 Console.WriteLine( mini);
 //if (num > max)
-//{
+//
 //    max = num;
 //}
