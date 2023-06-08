ea4d66d1adeda4064e59394ee8e8271d679a1aa5
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-06-08 18:30:05 +0530

    To Merge 2 int array into one.

--- ArrayDemo/Program.cs
+++ ArrayDemo/Program.cs
@@ -17,19 +17,77 @@
 //    Console.WriteLine(marks[i]);
 //}
 
-//get value from the user
+//get value from the user and print at the end
 
-int[] marks = new int[5];
+//int[] marks = new int[5];
 
-    for (int i = 0; i < 5; i++)
-    {
-        Console.WriteLine("enter a value");
-        marks[i] = Convert.ToInt32(Console.ReadLine());
+//    for (int i = 0; i < marks.Length; i++)
+//    {
+//        Console.WriteLine("enter a value");
+//        marks[i] = Convert.ToInt32(Console.ReadLine());
+
+//}
+////int sum = 0;
+//int maxi = 0;
+//int mini = 0;
+//    for (int i = 0; i < marks.Length; i++)// this is for after print
+//{
+//    //Console.WriteLine(marks[i]);
+//    // sum = sum + marks[i];// total print
+
+//    //if (maxi < marks[i])
+//    //{
+//    //    maxi = marks[i]; 
+//    //}
+//    if (i == 1)
+//    {
+//        mini = marks[i];
+//    }
+//    if (marks[i]<mini)
+//    {
+//        mini = marks[i];
+
+//    }
+
+//}
+////Console.WriteLine(sum);
+////Console.WriteLine(maxi);
+//Console.WriteLine(mini);
+
+//To Marge 2 int array into one.
+
+using System.Xml.Schema;
+
+int[] A = new int[5];
+int[] B = new int[5];
+int[] C = new int[10];
+
+
+for (int i = 0;i < A.Length;i++)
+{
+    Console.WriteLine("Enter a Value");
+    A[i] = Convert.ToInt32(Console.ReadLine());
+    C[i] = A[i];
    
 }
-    for (int i = 0; i<5; i++)
+for(int i = 0;i < B.Length; i++)
 {
-    Console.WriteLine(marks[i]);
+
+    Console.WriteLine("Again value for B");
+    B[i] = Convert.ToInt32(Console.ReadLine());
+    C[A.Length+i] = B[i];
+
 }
-  
- 
+for (int i = 0; i < C.Length; i++)
+{
+    Console.WriteLine(C[i]);
+    
+}
+
+
+
+
+
+
+
+
