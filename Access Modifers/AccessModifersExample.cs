5ddb8162bb64ad2d67b917a5dffed325376b926d
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-06-22 09:45:52 +0530

    Accessmodifer inside used public private and internal modifers

---
+++ Access Modifers/AccessModifersExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifers
+{
+    internal class AccessModifersExample
+    {
+        public String car;
+        private string car2;
+        internal string car3;
+    }
+}
---
+++ Access Modifers/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using Access_Modifers;
+
+AccessModifersExample Acc = new AccessModifersExample();
+
+Acc.car = "Maruti";
+Acc.car3 = "hyudai";
