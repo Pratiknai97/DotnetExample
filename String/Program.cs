49326466a88b5179443a5a9f5c014f7d6f4c071f
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-06-22 10:09:26 +0530

    IndexOf() by using string

--- String/Program.cs
+++ String/Program.cs
@@ -36,18 +36,19 @@
 //{
 //    Console.WriteLine(str1 + "and" + str5 + "are not equal");
 //}
-string data = "BBC";
-string data2 = "ABC";
-int res = data.CompareTo(data2);
-Console.WriteLine(res);
-data = "ncoisnonKLNSVIN";
+// toupper case and lower case
+//string data = "BBC";
+//string data2 = "ABC";
+//int res = data.CompareTo(data2);
+//Console.WriteLine(res);
+//data = "ncoisnonKLNSVIN";
 //data2=data.ToUpper();
 //Console.Write(data2);
 //data2=data.ToLower();
 //Console.Write(data2);
 
 //this is for trimStart () function
-data = "  A   AB   ABC   ";
+//data = "  A   AB   ABC   ";
 //data2 = data.TrimStart();
 //Console.WriteLine(data);
 //Console.WriteLine(data2);
@@ -56,17 +57,41 @@ data = "  A   AB   ABC   ";
 
 //this is for trimEnd();
 
-data2 = data.TrimEnd();
-Console.WriteLine(data);
-Console.WriteLine(data2);
-Console.WriteLine(data.Length);
-Console.WriteLine(data2.Length);
+//data2 = data.TrimEnd();
+//Console.WriteLine(data);
+//Console.WriteLine(data2);
+//Console.WriteLine(data.Length);
+//Console.WriteLine(data2.Length);
 
 //new this is for used for data trim
 
-data2 = data.Trim();
-Console.WriteLine(data);
-Console.WriteLine(data2);
-Console.WriteLine(data.Length);
-Console.WriteLine(data2.Length);
+//data2 = data.Trim();
+//Console.WriteLine(data);
+//Console.WriteLine(data2);
+//Console.WriteLine(data.Length);
+//Console.WriteLine(data2.Length);
 
+//new index of mathed
+
+
+        string str = "Helloworld";
+
+        int index1 = str.IndexOf('o');
+
+        Console.WriteLine("The index value of character 'F' is "+index1);
+   
+       int index2 = str.IndexOf('K');
+       Console.WriteLine("This value of Character 'K' is " + index2);
+
+// new way to find index method
+
+string str2 = "HelloWorld";
+
+// Finding the index of character
+// which is present in string
+// this will show the value 0
+int index3 = str2.IndexOf("H",0);
+Console.WriteLine("The index value of character 'H'" + "With start index 0 is" + index3);
+
+int index4 = str2.IndexOf("H", 5);
+Console.WriteLine("The index value of character 'H'" + "With start index 5 is" + index4);
