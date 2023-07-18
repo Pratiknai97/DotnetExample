24c5e7dc6a8c11948e126272cddaf278c140d90d
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-07-18 10:51:00 +0530

    Create interface with Example

---
+++ InterfaceExample1/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceExample1;
+
+// IShape Interface is only used for implementation of Anyshape not for any perticual shape and
+// IShape Ishape = new IShape(); //interface not for memory location.
+string Ans; 
+Square s1 = new Square();
+Ans = s1.Draw();
+Console.WriteLine(Ans);
+
+//New same thing for Rectangle
+
+Rectangle s2 = new Rectangle();
+Ans = s2.Draw();
+Console.WriteLine(Ans);
+
+IShape s3 = s1;
+Console.WriteLine(s3.Draw());
+
+s3 = s2;
+Console.WriteLine(s3.Draw());
---
+++ InterfaceExample1/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample1
+{
+    internal class Rectangle : IShape
+    {
+        public int length { get; set; }
+        public int width { get; set; }
+
+        public string Draw()
+        {
+            return "Rectangle";
+        }
+    }
+}
