db86f2e99d28bbbfb226fab89dc1482e78e2111d
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-07-21 12:41:37 +0530

    Collection by using List And lambda expression and by using this how to find out rates and Id easily

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -13,7 +13,8 @@
 //{
 //    Console.WriteLine(i);
 //}
-//list[0] = 50;
+//list[0] = 25;
+//Console.WriteLine(list[0]);
 //list.RemoveAt(0);
 //Console.WriteLine(list.Count);
 //list.Clear();
@@ -21,21 +22,26 @@
 
 using Collection;
 
-List<Product> products = new List<Product>() ;
+List<Product> products = new List<Product>();
 
-products.Add(new Product { ID = 1,Name = "Laptop", Rate = 35000} ) ;
-products.Add(new Product { ID = 2, Name = "Mouse", Rate = 35000 });
-products.Add(new Product { ID = 3, Name = "Keyboard", Rate = 35000 });
+products.Add(new Product { ID = 1, Name = "Laptop", Rate = 35000 });
+products.Add(new Product { ID = 2, Name = "Mouse", Rate = 15000 });
+products.Add(new Product { ID = 3, Name = "Keyboard", Rate = 45000 });
 
-foreach(Product product in products)
+foreach (Product product in products)
 {
     Console.WriteLine($"Product:{product.ID},Name:{product.Name},Rate:{product.Rate}");
 }
+var Result = products.Where(x=>x.Rate>20000).ToList();
+Console.WriteLine(Result.Count);
+foreach(Product product in Result)
+{
+    Console.WriteLine($"ID:{product.ID},Name:{product.Name},Rate:{product.Rate}");
+}
 
+var a = products.Where(x=>x.ID==1).FirstOrDefault();
 
-
-
-
+Console.WriteLine($"ID:{a.ID},Name:{a.Name},Rate:{a.Rate}");
 
 
 
