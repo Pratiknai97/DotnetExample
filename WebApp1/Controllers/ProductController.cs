cfcbc68afb6480f52ca84faec459084b492d68cf
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-08-02 19:30:21 +0530

    Add Two product by using List and View list of products

---
+++ WebApp1/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp1.Model;
+
+namespace WebApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    
+    {
+        [HttpGet]
+        public IActionResult Getdata()
+        {
+            Product p1 = new Product();
+            p1.ID = 1;
+            p1.PName = "KeyBoard";
+            p1.Rate = 1500;
+            Product p2 = new Product();
+            p2.ID = 2;
+            p2.PName = "Mouse";
+            p2.Rate = 1000;
+            List<Product> products = new List<Product>();
+            products.Add(p1);
+            products.Add(p2);
+            
+            return Ok(products);
+        }
+    }
+}
---
+++ WebApp1/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp1.Model
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string PName { get; set; }
+        public int Rate { get; set; }
+    }
+}
