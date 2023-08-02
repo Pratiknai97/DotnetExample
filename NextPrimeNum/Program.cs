f366c5bdc32d544ade73e4e265323fd9f06e7851
Pratiknai97 <133839234+Pratiknai97@users.noreply.github.com>
2023-08-02 18:45:58 +0530

    Print Next prime Number

---
+++ NextPrimeNum/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a number");
+int x = Convert.ToInt32(Console.ReadLine());
+int nextPrime = x + 1;
+
+while (true)
+{
+    int count = 0;
+
+    for (int i = 1; i <= nextPrime; i++)
+    {
+        if (nextPrime % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if (count == 2)
+    {
+        Console.WriteLine("Next prime number: " + nextPrime);
+        break;
+    }
+
+    nextPrime++;
+}
